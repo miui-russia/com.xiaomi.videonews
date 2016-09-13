bcdebfbcea8c24266cfe09fd837d5837a2d819b2
xiaominame <1521004750@qq.com>
2016-09-13 09:41:34 +0800

    vitamio添加

---
+++ app/src/main/java/com/xiaomi/videonews/MainActivity.java
@@ -0,0 +1,18 @@
+package com.xiaomi.videonews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import io.vov.vitamio.MediaPlayer;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
